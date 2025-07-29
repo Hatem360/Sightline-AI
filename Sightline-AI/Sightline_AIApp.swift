2d1a5cdba6d93bce03c0ea8564a38380046c34b8
Hatem Karoui <karouih602@icloud.com>
2025-07-29 15:58:34 +0200

    Initial Commit

---
+++ Sightline-AI/Sightline_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sightline_AIApp.swift
+//  Sightline-AI
+//
+//  Created by Hatem Karoui on 29.07.25.
+//
+
+import SwiftUI
+
+@main
+struct Sightline_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
